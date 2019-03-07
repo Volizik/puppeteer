7c378506db3268317242e859aaa74658286c83f4
Yura <yurafront@gmail.com>
2019-03-07 14:13:03 +0300

    add client

---
+++ client/src/views/SitesList.vue
@@ -0,0 +1,95 @@
+<template>
+  <v-container fill-height fluid grid-list-xl>
+    <v-layout justify-center wrap>
+      <v-flex xs12 md8>
+        <material-card color="green" title="Добавление страницы">
+          <v-form>
+            <v-container py-0>
+              <v-layout wrap>
+                <v-flex xs12 md12 >
+                  <v-text-field label="URL страницы" class="purple-input" />
+                </v-flex>
+                <v-flex xs6 md2 >
+                  <v-text-field class="purple-input" label="Просмотры" type="number" />
+                </v-flex>
+                <v-flex xs6 md2 >
+                  <v-text-field class="purple-input" label="Досмотры" type="number" />
+                </v-flex>
+                <v-flex xs12 text-xs-right>
+                  <v-btn class="mx-0 font-weight-light" color="success">Добавить сайт</v-btn>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-form>
+        </material-card>
+      </v-flex>
+      <v-flex md12>
+        <material-card color="green" title="Список" text="Here is a subtitle for this table">
+          <v-data-table :headers="headers" :items="items" hide-actions>
+            <template slot="headerCell" slot-scope="{ header }">
+              <span class="subheading font-weight-light text-success text--darken-3" v-text="header.text"></span>
+            </template>
+            <template slot="items" slot-scope="{ item }">
+              <td><img :src="item.logo" :alt="item.name" height="25" width="25"></td>
+              <td>{{ item.name }}</td>
+              <td>{{ item.url }}</td>
+              <td>{{ item.views }}</td>
+              <td>{{ item.scroll }}</td>
+              <td class="text-xs-right">{{ item.status }}</td>
+            </template>
+          </v-data-table>
+        </material-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      headers: [
+        {
+          sortable: false,
+          text: 'Лого',
+          value: 'logo'
+        },
+        {
+          sortable: false,
+          text: 'Название страницы',
+          value: 'name'
+        },
+        {
+          sortable: false,
+          text: 'Адресс страницы',
+          value: 'url'
+        },
+        {
+          sortable: false,
+          text: 'Просмотры',
+          value: 'views'
+        },
+        {
+          sortable: false,
+          text: 'Досмотры',
+          value: 'scroll'
+        },
+        {
+          sortable: false,
+          text: 'Статус',
+          value: 'status',
+          align: 'right'
+        }
+      ],
+      items: [
+        {
+          logo: 'https://cdn4.iconfinder.com/data/icons/new-google-logo-2015/400/new-google-favicon-512.png',
+          name: 'Google',
+          url: 'www.google.com',
+          views: '10',
+          scroll: '20',
+          status: 'Выполнено'
+        }
+      ]
+    })
+  }
+</script>
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const Koa = require('koa');
 const send = require('koa-send');
+const serve = require('koa-static');
+
 const apiRouter = require('./routes/api');
 const app = new Koa();
 
 app.use(apiRouter.routes());
-app.use(async (ctx) => await send(ctx, 'index.html', { root: __dirname + '/client' }));
+
+app.use(serve('./client/dist'));
+app.use(async (ctx) => await send(ctx, 'index.html', { root: __dirname + '/client/dist' }));
 
 app.listen(8080, () => console.log('Server is running!'));
